2949d45215a46fab7f1e1b6aabc7bdfe4ceff812
OctopusHugz <colsonscott53@gmail.com>
2021-04-10 19:59:22 +0000

    Changed getenv(PWD) to getcwd()

--- cwd.py
+++ cwd.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from os import getenv, path
+from os import getcwd, path
 
 
 def pid_from_cwd():
     """Returns a project's ID based on the current working directory"""
-    cwd = path.basename(getenv('PWD'))
+    cwd = path.basename(getcwd())
     sedo_list = [{205: "0x00-shell_basics"},
                  {207: "0x01-shell_permissions"}, {208: "0x02-shell_redirections"}, {209: "0x03-shell_variables_expansions"}, {251: "0x04-loops_conditions_and_parsing"}, {255: "0x05-processes_and_signals"}, {78: "0x06-regular_expressions"}, {259: "0x07-networking_basics"}, {285: "0x08-networking_basics_2"}, {302: "0x09-web_infrastructure_design"}, {292: "0x0A-configuration_management"}, {244: "0x0B-ssh"}, {266: "0x0C-web_server"}, {265: "0x0D-web_stack_debugging_0"}, {271: "0x0E-web_stack_debugging_1"}, {275: "0x0F-load_balancer"}, {276: "0x10-https_ssl"}, {298: "0x11-what_happens_when_your_type_holbertonschool_com_in_your_browser_and_press_enter"}, {287: "0x12-web_stack_debugging_2"}, {284: "0x13-firewall"}, {280: "0x14-mysql"}, {269: "0x15-api"}, {314: "0x16-api_advanced"}, {293: "0x17-web_stack_debugging_3"}, {281: "0x18-webstack_monitoring"}, {294: "0x19-postmortem"}, {311: "0x1A-application_server"}, {313: "0x1B-web_stack_debugging_4"}]
     low_list = [{212: "0x00-hello_world"},
@@ -32,5 +32,5 @@ def pid_from_cwd():
 
 def parent_from_cwd():
     """Returns the parent directory based on the current working directory"""
-    parent = getenv('PWD').split('/')[-2]
+    parent = getcwd().split('/')[-2]
     return parent
