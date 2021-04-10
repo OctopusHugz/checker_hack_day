827253b44fa16358fa59f3a7808632d8aad456b3
OctopusHugz <colsonscott53@gmail.com>
2021-04-10 19:24:03 +0000

    Fixed main loop to only import request_correction if authorized

--- check_task.py
+++ check_task.py
@@ -2,7 +2,7 @@
 """This module runs the gitcheck program"""
 if __name__ == "__main__":
     from auth_test import settings_auth_test
-    from task_corrections import request_correction
     authorized = settings_auth_test()
     if authorized:
+        from task_corrections import request_correction
         request_correction()
