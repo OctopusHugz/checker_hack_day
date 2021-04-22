1f9f47ec80adc0af82960f6c3938f6df9ae785de
OctopusHugz <colsonscott53@gmail.com>
2021-04-22 18:21:42 +0000

    Added updates to task_corrections.py. Need to refactor using async/await for robustness

--- task_corrections.py
+++ task_corrections.py
@@ -2,8 +2,8 @@
 from cwd import pid_from_cwd
 from sys import argv
 from settings import auth_token
+from time import sleep
 import requests
-import time
 from colorama import Fore, init
 checks_dict = {}
 payload = {'Content-Type': 'application/json'}
@@ -12,7 +12,13 @@ init(autoreset=True)
 
 def request_correction():
     """Requests a correction given a task ID"""
+    if len(argv) > 2:
+        print(Fore.RED + "Try running with no arguments, or a single task number!")
+        exit(1)
     project_id = pid_from_cwd()
+    if project_id is None:
+        print(Fore.RED + "Whoops! No checker for you today. Try a manual review instead!")
+        exit(1)
     task_url = 'https://intranet.hbtn.io/projects/{}.json?auth_token={}'.format(project_id,
                                                                                 auth_token)
     project = requests.get(task_url, params=payload).json()
@@ -22,28 +28,46 @@ def request_correction():
         exit(1)
     task_list = []
     task_files = []
+    non_zero_start = False
+    one_arg_given = True if len(argv) == 2 else False
     for task in tasks:
-        if task.get('checker_available'):
-            task_list.append(task)
+        task_list.append(task)
         task_files.append(task.get('github_file'))
-    if len(argv) > 1:
-        for filename in argv[1:]:
-            if filename not in task_files:
-                print(Fore.RED + 'You have an invalid filename. Exiting...')
-                exit(1)
-    if len(task_list) == 0:
+    if one_arg_given:
+        try:
+            task_number = int(argv[1])
+        except Exception:
+            print(Fore.RED + "Try passing in a task number instead!")
+            exit(1)
+    checker_available_list = [task.get("checker_available") for task in tasks]
+    if True not in checker_available_list:
         print(
-            Fore.RED + 'This task has no checker. Please find a peer for a manual review!')
+            Fore.RED + 'This project has no checker. Please find a peer for a manual review!')
         exit(1)
-    # See behavior with 0x15 JS Web Query, giving 3-js and 3.html results in different output
+
+    if task_list[0].get('position') != 0:
+        non_zero_start = True
+
+    if one_arg_given:
+        if checker_available_list[task_number]:
+            # try:
+            correct_task(task_list[task_number].get("id"),
+                         task_list[task_number].get("title"),
+                         task_number)
+            # except Exception:
+            #     print(
+            #         Fore.RED + "Looks like this task has no checker! Please find a peer for a manual review!")
+        else:
+            print(
+                Fore.RED + "Looks like this task has no checker! Please find a peer for a manual review!")
+
     for task in task_list:
         task_id = task.get('id')
         task_title = task.get('title')
-        task_file = task.get('github_file')
         task_position = task.get('position')
-        if task_position < 100 and task_list[0].get('position') != 0:
+        if task_position < 100 and non_zero_start:
             task_position -= 1
-        if (len(argv) > 1 and task_file in argv) or len(argv) == 1:
+        if len(argv) == 1 and task.get("checker_available"):
             correct_task(task_id, task_title, task_position)
 
 
@@ -53,6 +77,8 @@ def correct_task(task_id, task_title, task_position):
     correction_res_id = requests.post(
         correction_url, params=payload).json().get('id')
     if not correction_res_id:
+        # or could be manual review?!
+        # breaking when running ct only and manual review in middle of autos?
         print('Too many requests...Please respect the rate limits!')
         exit(1)
     print("Waiting on correction result for task: " + Fore.BLUE + "{}. {}".format(
@@ -71,7 +97,8 @@ def update_checks_dict(correction_result_url, task_id):
         correction_result_url, params=payload).json()
     status = correction_result.get('status')
     while status == 'Sent':
-        time.sleep(2.5)
+        # turn this sleep into async/await or something more robust?
+        sleep(2)
         correction_result = requests.get(
             correction_result_url, params=payload).json()
         status = correction_result.get('status')
@@ -99,7 +126,5 @@ def update_checks_dict(correction_result_url, task_id):
         code_color = Fore.GREEN
     else:
         code_color = Fore.RED
-    print(req_color +
-          'Requirement Checks: {:d}/{:d}'.format(req_dict_passed, len(req_dict)))
-    print(code_color +
-          'Code Checks: {:d}/{:d}'.format(code_dict_passed, len(code_dict)))
+    print(req_color + f'Requirement Checks: {req_dict_passed}/{len(req_dict)}')
+    print(code_color + f'Code Checks: {code_dict_passed}/{len(code_dict)}')
